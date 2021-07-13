8e7f722ad965e984d373e736ff07f20ec2c02fe4
Nima <pn2120551@gmail.com>
2021-07-13 22:46:09 +0430

    Creating project and adding files

---
+++ cpp/basic.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <map>
+#include <set>
+#include <math.h>
+#include <string>
+#include <iomanip>
+
+#define deb(x); cout << "**DEB** " << #x << " = " << x << '\n';
+#define pr(x); for( auto it = x.begin() ; it != x.end() ; it++) { cout << *it << " ";} cout << '\n';
+#define prr(x); for( auto it = x.rbegin() ; it != x.rend() ; it++) { cout << *it << " ";} cout << '\n';
+
+#define PI 3.14159265358979323846 
+#define ull unsigned long long
+#define ll long long
+
+using namespace std;
+
+void solve(){
+	
+}
+
+int main(){
+
+	std::ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	solve();
+	return 0;
+}
